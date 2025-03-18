69d3f30642d6a2029a4a57d702722fc158daae4b
Tian <143532687+pxisbest@users.noreply.github.com>
2025-03-18 23:15:21 +0800

    first commit-import tailwind css and shadcnui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+  <h1 className="text-red-500">hello</h1>
+  <Button variant={"secondary"}>Button</Button>
+  </>
+  )
+}
+
+export default App
