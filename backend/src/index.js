1e8e42a1ea816ab7c6f1efac9b949c30bd981f23
Tian <143532687+pxisbest@users.noreply.github.com>
2025-04-01 14:50:40 +0800

    model the database

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import connectDB from './lib/db.js';
+
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import songRoutes from './routes/song.route.js';
+import albumRoutes from './routes/album.route.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 5004;
-
 
 app.use("/api/users",userRoutes);
 app.use("/api/auth",authRoutes);
@@ -15,4 +20,6 @@ app.use("/api/albums",albumRoutes);
 
 app.listen(PORT,()=>{
     console.log('Server is running on port' + PORT);
+    connectDB();
+    console.log('Connecting to MongoDB URI:', process.env.MONGODB_URI)
 })
